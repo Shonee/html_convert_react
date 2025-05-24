818945dcfb8e8a1a14b068870de7748b0bcc3a72
Adu <1048394082@qq.com>
2025-05-24 23:33:51 +0800

    Add files via upload

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>HTML 转换工具 &copy; {new Date().getFullYear()} - 支持多种格式转换</p>
+    </div>
+  );
+};
+
+export default Footer;
