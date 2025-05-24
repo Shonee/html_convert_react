292dc54ce9a46bb5a6c0df60c6be20aa4345938d
Adu <1048394082@qq.com>
2025-05-24 23:31:47 +0800

    Create Header.jsx

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>HTML 转换工具</h1>
+      <p>一站式 HTML 文件转换解决方案</p>
+    </div>
+  );
+};
+
+export default Header;
